6691f1ed47c666a35c88c95000a055470b4e55b5
josephinemagu <77861418+josephinemagu@users.noreply.github.com>
2021-02-14 01:25:57 +0300

    Update for.py

--- source/week-2/loops/for.py
+++ source/week-2/loops/for.py
@@ -1,2 +1,7 @@
-for name in ['John Doe', 'Jane Doe']:
-	print(name)
+
+people= ['John Doe', 'Jane Doe', 'Josephine Magu']
+for name in range(len(people)):
+	print('Current person:', people[index]))
+
+	
+
