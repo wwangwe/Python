cc01ef0d8ac5d0c64cb54f5c76724452f5d48834
Timothy Wangwe <wangwetymothy@gmail.com>
2021-02-18 12:32:37 +0300

    Restart clean project

--- Week_1/source/error-handling/logic.py
+++
@@ -1,6 +0,0 @@
-x = 45
-y = 206
-if x < y:
-    print(str(x) + ' is greater than ' + str(y))
-else:
-    print('all cool')
--- source/week-2/loops/while.py
+++
@@ -1,6 +0,0 @@
-names = ['John Doe', 'Jane Doe']
-index = 0
-while index < len(names):
-	print(names[index])
-	# Change the condition!!
-	index = index + 1
